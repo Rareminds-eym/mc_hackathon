aaad13d1413f6a5df4f58a45afbcc246d751be41
Sinjucodes <sinjpl41@gmail.com>
2025-07-04 00:12:44 +0530

    Browser controllers state management

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,6 +4,16 @@ import audioReducer from './slices/audioSlice';
 import uiReducer from './slices/uiSlice';
 import progressReducer from './slices/progressSlice';
 import bingoReducer from './slices/bingoSlice';
+// redux-persist imports
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage
+
+const bingoPersistConfig = {
+  key: 'bingo',
+  storage,
+};
+
+const persistedBingoReducer = persistReducer(bingoPersistConfig, bingoReducer);
 
 export const store = configureStore({
   reducer: {
@@ -11,13 +21,13 @@ export const store = configureStore({
     audio: audioReducer,
     ui: uiReducer,
     progress: progressReducer,
-    bingo: bingoReducer,
+    bingo: persistedBingoReducer, // use persisted reducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         // Ignore these action types for Set serialization
-        ignoredActions: ['game/completeLevel', 'game/loadProgress'],
+        ignoredActions: ['game/completeLevel', 'game/loadProgress', 'persist/PERSIST'],
         // Ignore these field paths in all actions
         ignoredActionsPaths: ['payload.completedLevels'],
         // Ignore these paths in the state
@@ -25,6 +35,8 @@ export const store = configureStore({
       },
     }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
