9c1f055ff546a2a9d5d7403a29374d0a30773179
karthikeyansrm <karthikeyan@rareminds.in>
2025-08-15 23:15:47 +0530

    upshed the module update

--- src/data/modules.ts
+++ src/data/modules.ts
@@ -1,10 +1,10 @@
 import type { Module } from '../types/module';
 
 export const modules: Module[] = [
-  { id: "1", status: 'available', title: 'Introduction to GMP', progress: 0 },
-  { id: "2", status: 'available', title: 'Personal Hygiene' },
-  { id: "3", status: 'available', title: 'Cleaning Validation' },
-  { id: "4", status: 'available', title: 'Documentation' },
+  { id: "1", status: 'locked', title: 'Medical Terminology & Anatomy', progress: 0 },
+  { id: "2", status: 'locked', title: 'CPT & HCPCS Fundamentals' },
+  { id: "3", status: 'locked', title: 'ICD-10-CM Guidelines & Structure' },
+  { id: "4", status: 'locked', title: 'CPT & HCPCS Coding' },
   { id: "HL1", status: 'available', title: 'Hackathon Level-1' },
   { id: "HL2", status: 'locked', title: 'Hackathon Level-2' },
 ];
